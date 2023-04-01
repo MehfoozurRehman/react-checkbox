026fe2cea2c187dfc0eba3d62dd412982c04885b
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-04-02 01:14:39 +0500

    clean up and deps updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,57 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import "./styles.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const CheckBox = ({
+  onChange,
+  value,
+  checkedColor,
+  unCheckedColor,
+  iconColor,
+  label,
+  labelColor,
+  style,
+  ...props
+}) => {
+  return (
+    <div className="checkbox">
+      <input
+        class="checkbox__input"
+        type="checkbox"
+        value={value}
+        onChange={onChange}
+        {...props}
+      />
+      <div className="checkbox__content">
+        <div
+          className="checkbox__content__indicator"
+          style={
+            value
+              ? { background: checkedColor, color: iconColor, ...style }
+              : { background: unCheckedColor, ...style }
+          }
+        >
+          {value ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-check"
+            >
+              <polyline points="20 6 9 17 4 12"></polyline>
+            </svg>
+          ) : null}
+        </div>
+        <div className="checkbox__content__label" style={{ color: labelColor }}>
+          {label}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default CheckBox;
